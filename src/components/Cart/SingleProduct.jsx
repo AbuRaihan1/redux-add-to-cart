cd0df2e503f6e3b6f378eff06c768b8a3dc76d20
Abu Raihan <raihanahmed01973@gmail.com>
2024-05-30 11:53:16 +0600

    add toast, when added product

--- src/components/Cart/SingleProduct.jsx
+++ src/components/Cart/SingleProduct.jsx
@@ -1,19 +1,42 @@
-import React from "react";
-import { Card } from "flowbite-react";
+import React, { useState } from "react";
+import { Card, Toast } from "flowbite-react";
+import { HiCheck } from "react-icons/hi";
 import { useDispatch, useSelector } from "react-redux";
 import { addToCart } from "../../redux/features/AddToCartSlice";
 import { handleDrawerOpen } from "../../redux/features/CartDrawerSlice";
+
 const SingleProduct = ({ product }) => {
   const { name, price, rating, description, inStock, img } = product;
   const dispatch = useDispatch();
+  const [showToast, setShowToast] = useState(false);
+
+  const handleShowToast = () => {
+    setShowToast(true);
+    setTimeout(() => {
+      setShowToast(false);
+    }, 3000);
+  };
 
   const addToCartProduct = () => {
     dispatch(addToCart(product));
+    handleShowToast();
     dispatch(handleDrawerOpen());
   };
 
   return (
     <div className="border p-4">
+      {showToast && (
+        <Toast className="border fixed bottom-3 left-3 z-50 mx-auto">
+          <div className="inline-flex h-8 w-8 shrink-0 items-center justify-center rounded-lg bg-cyan-100 text-cyan-500 dark:bg-cyan-800 dark:text-cyan-200">
+            <HiCheck className="h-5 w-5" />
+          </div>
+          <div className="ml-3 text-sm font-normal">
+            <span className="font-bold text-green-400">{product?.name || cart}</span> Added
+            Successfully
+          </div>
+          <Toast.Toggle onDismiss={() => setShowToast(false)} />
+        </Toast>
+      )}
       <img src={img} alt={name} className="w-full h-[200px]" />
       <a href="#">
         <h5 className="text-xl font-semibold tracking-tight text-gray-900 dark:text-white mt-2">
